a48ff702b7da345c5b2393e3fa1df517f23ee62f
Kyryl <sautakiki.com@gmail.com>
2026-04-24 14:07:07 +0300

    sol

--- cinema/views.py
+++ cinema/views.py
@@ -29,9 +29,12 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all()
+    queryset = MovieSession.objects.all().select_related("movie", "cinema_hall")
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type[
+        MovieSessionListSerializer |
+        MovieSessionDetailSerializer |
+        MovieSessionSerializer]:
         if self.action == "list":
             return MovieSessionListSerializer
         if self.action == "retrieve":
@@ -45,9 +48,13 @@ class TicketViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.all().prefetch_related("actors", "genres")
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type[
+        MovieListSerializer |
+        MovieDetailSerializer |
+        MovieSerializer
+    ]:
         if self.action == "list":
             return MovieListSerializer
         if self.action == "retrieve":
